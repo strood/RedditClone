a4000e88dc81ef36634b72cfede9e1feba93a2d4
strood <stroodz19@gmail.com>
2020-08-11 11:45:24 -0600

    small fixes

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,20 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("packs/script")
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true))
-
 window.test_func = function toggleReplyBox(id) {
   if(document.getElementById('reply_box' + id).classList.contains('hidden'))
     document.getElementById('reply_box' + id).classList.remove('hidden');
